295c2340add83dd77a4eae66ce3101ffb6378e8a
PRINCE TECH <princemayel86@gmail.com>
2025-03-14 15:55:09 +0100

    Update

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 /*
-_  ______   _____ _____ _____ _   _
-| |/ / ___| |_   _| ____/___ | | | |
-| ' / |  _    | | |  _|| |   | |_| |
-| . \ |_| |   | | | |__| |___|  _  |
-|_|\_\____|   |_| |_____\____|_| |_|
-
-ANYWAY, YOU MUST GIVE CREDIT TO MY CODE WHEN COPY IT
-CONTACT ME HERE +237656520674
-YT: KermHackTools
-Github: Kgtech-cmr
+_  
 */
 
 const {
