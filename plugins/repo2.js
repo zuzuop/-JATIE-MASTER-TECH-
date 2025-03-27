7e201d321b90dee74ef57fd389993ed4a00c71fd
PRINCE TECH <princemayel86@gmail.com>
2025-03-27 22:15:25 +0100

    Update repo2.js

--- plugins/repo2.js
+++ plugins/repo2.js
@@ -10,13 +10,13 @@ cmd({
   use: ".repo",
   filename: __filename
 }, 
-async (conn, mek, m, { from, reply, pushName }) => {
+async (conn, mek, m, { from, reply, pushname }) => {
   try {
     const repoUrl = "https://api.github.com/repos/mayelprince/PRINCE-MDXI";
     const response = await axios.get(repoUrl);
     const { stargazers_count, forks_count, created_at, updated_at } = response.data;
 
-    const repoInfo = `Hello *${pushName}*,\n\nThis is *PRINCE-MDXI,* A WhatsApp Bot Built by *PRINCE TECH,* Enhanced with Amazing Features to Make Your WhatsApp Communication and Interaction Experience Amazing.\n\n` +
+    const repoInfo = `Hello *${pushname}*,\n\nThis is *PRINCE-MDXI,* A WhatsApp Bot Built by *PRINCE TECH,* Enhanced with Amazing Features to Make Your WhatsApp Communication and Interaction Experience Amazing.\n\n` +
       `*ʀᴇᴘᴏ ʟɪɴᴋ:* https://github.com/mayelprince/PRINCE-MDXI\n\n` +
       `*❲❒❳ ɴᴀᴍᴇ:* PRINCE-MDXI\n` +
       `*❲❒❳ ⭐ sᴛᴀʀs:* ${stargazers_count}\n` +
