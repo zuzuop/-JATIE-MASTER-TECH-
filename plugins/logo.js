79beacc2542dce2184b7e83b75220236c27bf481
PRINCE TECH <princemayel86@gmail.com>
2025-03-31 19:18:16 +0100

    BY PRINCE

--- plugins/logo.js
+++ plugins/logo.js
@@ -34,3 +34,31 @@ cmd({
         reply("⚠️ *An error occurred while fetching the anime. Please try again later.*");
     }
 });
+
+
+cmd({
+  pattern: "rw",
+  alias: ["randomwall", "wallpaper"],
+  react: "🌌",
+  desc: "Download random wallpapers based on keywords.",
+  category: "random",
+  use: ".rw <keyword>",
+  filename: __filename
+}, async (conn, m, store, { from, args, reply }) => {
+  try {
+    const query = args.join(" ") || "random";
+    const apiUrl = `https://pikabotzapi.vercel.app/random/randomwall/?apikey=anya-md&query=${encodeURIComponent(query)}`;
+
+    const { data } = await axios.get(apiUrl);
+    
+    if (data.status && data.imgUrl) {
+      const caption = `🌌 *Random Wallpaper: ${query}\n> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴘʀɪɴᴄᴇ ᴛᴇᴄʜ  `;
+      await conn.sendMessage(from, { image: { url: data.imgUrl }, caption }, { quoted: m });
+    } else {
+      reply(`❌ No wallpaper found for *"${query}"*.`);
+    }
+  } catch (error) {
+    console.error("Wallpaper Error:", error);
+    reply("❌ An error occurred while fetching the wallpaper. Please try again.");
+  }
+});
