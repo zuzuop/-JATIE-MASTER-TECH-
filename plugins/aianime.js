a4fa0cf920f239b5c4d161f6b991647f0f8fb9fe
PRINCE TECH <princemayel86@gmail.com>
2025-03-31 05:40:47 +0100

    BY PRINCE

---
+++ plugins/aianime.js
@@ -0,0 +1,30 @@
+const { cmd, commands } = require('../command');
+const axios = require('axios');
+
+cmd({
+    pattern: "hentai",
+    desc: "Sends a random hentai video.",
+    category: "anime",
+    filename: __filename,
+}, async (conn, mek, m, { reply }) => {
+    try {
+        const response = await axios.get("https://apis.davidcyriltech.my.id/hentai");
+        if (!response.data) return reply("❌ No response from API.");
+
+        const data = response.data;
+        let videoUrl = data.video?.url || data.video?.link || data.video;
+
+        if (!videoUrl || typeof videoUrl !== 'string') {
+            return reply("❌ Could not extract video URL properly.");
+        }
+
+        const caption = "🎥 Here is your random video\n> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴘʀɪɴᴄᴇ ᴛᴇᴄʜ ";
+
+        // Envoyer directement la vidéo en utilisant le lien de l'API
+        await conn.sendMessage(m.chat, { video: { url: videoUrl }, caption: caption, mimetype: 'video/mp4' }, { quoted: m });
+
+    } catch (error) {
+        console.error(error);
+        reply("⚠️ An error occurred: " + error.message);
+    }
+});
