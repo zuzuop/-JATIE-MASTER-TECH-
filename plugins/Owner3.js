cfec008bf7cf6f06783ee20ef8280bd85fdc9995
PRINCE TECH <princemayel86@gmail.com>
2025-04-06 23:29:36 +0100

    BY PRINCE

---
+++ plugins/Owner3.js
@@ -0,0 +1,28 @@
+const { cmd } = require('../command');
+const config = require('../config');
+
+cmd({
+    pattern: "ownerr",
+    react: "✅", 
+    desc: "Get owner number",
+    category: "main",
+    filename: __filename
+}, 
+async (conn, mek, m, { from }) => {
+    try {
+        const ownerNumber = config.OWNER_NUMBER; // Fetch owner number from config
+        const ownerName = config.OWNER_NAME;     // Fetch owner name from config
+
+        const vcard = 'BEGIN:VCARD\n' +
+                      'VERSION:3.0\n' +
+                      `FN:${ownerName}\n` +  
+                      `TEL;type=CELL;type=VOICE;waid=${ownerNumber.replace('+', '')}:${ownerNumber}\n` + 
+                      'END:VCARD';
+
+        // Send the vCard
+        const sentVCard = await conn.sendMessage(from, {
+            contacts: {
+                displayName: ownerName,
+                contacts: [{ vcard }]
+            }
+        });
